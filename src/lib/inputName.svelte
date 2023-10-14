c5f5dbd50852b06d0bc59ba613bb1e3ead043059
Tomatillo1 <totofarkas@gmail.com>
2023-10-14 23:38:53 +0200

    choose players

---
+++ src/lib/inputName.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let whatPlayer
+    export let value
+</script>
+
+<label for="players">{whatPlayer}</label>
+<input type="text" name="players" class="players" placeholder="Nom du joueur" bind:value>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+    .players {
+        width: 30%;
+        background: black;
+        border: 1px solid white;
+        color: white;
+        font-size: 1rem;
+        font-family: "Poppins", sans-serif;
+        padding: 0.25rem 0.5rem 0.25rem 0.5rem;
+    }
+
+    .players::placeholder {
+        color: white;
+        opacity: 50%;
+        font-style: italic;
+        font-family: "Poppins", sans-serif;
+    }
+
+    label {
+        color: white;
+        font-family: "Poppins", sans-serif;
+        font-size: 1.5rem;
+    }
+</style>
